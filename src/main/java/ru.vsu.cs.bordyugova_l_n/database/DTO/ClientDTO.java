d1cd650034a053361d922448ebd2b0d47733a258
Amelia-coder <o24697692@gmail.com>
2025-01-18 22:24:27 +0300

    + dto

---
+++ src/main/java/ru.vsu.cs.bordyugova_l_n/database/DTO/ClientDTO.java
@@ -0,0 +1,18 @@
+package ru.vsu.cs.bordyugova_l_n.database.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientDTO {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private Integer room; // Only accept room ID as an integer
+    private String resortCard;
+}
--- src/main/java/ru.vsu.cs.bordyugova_l_n/web/ClientWebController.java
+++ src/main/java/ru.vsu.cs.bordyugova_l_n/web/ClientWebController.java
@@ -5,16 +5,21 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ru.vsu.cs.bordyugova_l_n.database.DTO.ClientDTO;
 import ru.vsu.cs.bordyugova_l_n.database.entities.Client;
+import ru.vsu.cs.bordyugova_l_n.database.entities.Room;
 import ru.vsu.cs.bordyugova_l_n.services.ClientService;
+import ru.vsu.cs.bordyugova_l_n.services.RoomService;
 
 @Controller
 @RequestMapping("/clients")
 public class ClientWebController {
     private final ClientService clientService;
+    private final RoomService roomService;
 
-    public ClientWebController(ClientService clientService) {
+    public ClientWebController(ClientService clientService, RoomService roomService) {
         this.clientService = clientService;
+        this.roomService = roomService;
     }
 
     @GetMapping
@@ -36,7 +41,20 @@ public class ClientWebController {
     }
 
     @PostMapping
-    public String addClient(@ModelAttribute Client client) {
+    public String addClient(@RequestBody ClientDTO clientDTO) {
+        Client client = new Client();
+        client.setFirstName(clientDTO.getFirstName());
+        client.setLastName(clientDTO.getLastName());
+        client.setPhone(clientDTO.getPhone());
+        client.setEmail(clientDTO.getEmail());
+        client.setResortCard(clientDTO.getResortCard());
+
+        // Fetch the Room entity by ID
+        if (clientDTO.getRoom() != null) {
+            Room room = roomService.getRoomById(Long.valueOf(clientDTO.getRoom()));
+            client.setRoom(room);
+        }
+
         clientService.addClient(client);
         return "redirect:/clients";
     }
